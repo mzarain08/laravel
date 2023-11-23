bc9746bf38518fe89095785a62d3ebd6309a730f
mzarain08 <mz_arain08@yahoo.com>
2023-11-23 17:26:33 +0500

    contact

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aboutus/{id}/{name}', function ($id, $name) {
+    $id = request('id');
+    $name = request('name');
+    echo $id . "==". $name;
+    die;
+});
+
+Route::resource('contacts',"ContactController");
+
+Route::get('/calculator', function () {
+    $n1 = request('number1');
+    $n2 = request('number2');
+    echo $n1 . "==". $n2;
+    die;
+});
